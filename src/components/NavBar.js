5514808b21ecffb481ffd42f3707917549ed2c3b
lorenzo <lorenzomojica561@gmail.com>
2023-09-13 17:35:36 -0400

    adds myfile.md

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,38 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const aTagList = links.map((link) => (<a key={link} href={`#${link}`}>{link}</a>))
+  //console.log(link)
+  //console.log(aTagList)
+
+
+  // const linksList = links.map((number, index) => {
+
+  return (
+     <nav>{/* display an <a> tag for each link here */}
+        {aTagList}
+
+     </nav>
+
+
+  )
+
+
+
+
+
+  //  <nav>{/* display an <a> tag for each link here */}
+  //     {/* <a href="#home">home</a>
+  //     <a href="#about">about</a>
+  //     <a href="#projects">projcets</a> */}
+
+  //  </nav>
+  
 }
 
-export default NavBar;
+export default NavBar; 
+
+
+// const linksList = links.map((number, index) => {
+
+//   return <nav key={index}>{number}</nav>
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,29 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  //console.log(projects);
+
+  //const aTagList = links.map((link) => (<a key={link}href='#{link}'>{link}</a>))
+
+
+  const projectItems = projects.map((item) => (
+    <ProjectItem 
+       key = {item.id}
+       about = {item.about}
+       id = {item.id}
+       name = {item.name}
+       technologies = {item.technologies}
+
+
+    
+    />
+
+  ))
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectItems}</div>
     </div>
   );
 }
